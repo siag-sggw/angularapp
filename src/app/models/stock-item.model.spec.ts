10752a43b3fa2aa45ad604fd8503df8986e16982
WiktorElAttar <wiktor.elattar@gmail.com>
2019-03-23 22:22:48 +0100

    added model tests

--- src/app/models/stock-item.model.spec.ts
+++ src/app/models/stock-item.model.spec.ts
@@ -2,6 +2,6 @@ import { StockItem } from './stock-item.model';
 
 describe('StockItem', () => {
   it('should create an instance', () => {
-    expect(new StockItem()).toBeTruthy();
+    expect(new StockItem('test', [])).toBeTruthy();
   });
 });
