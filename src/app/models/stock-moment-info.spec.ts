b8f28d34101e5c082f9cfa6df7013eabed3929c0
WiktorElAttar <wiktor.elattar@gmail.com>
2019-03-23 22:22:48 +0100

    added model tests

--- src/app/models/stock-moment-info.spec.ts
+++ src/app/models/stock-moment-info.spec.ts
@@ -2,6 +2,6 @@ import { StockMomentInfo } from './stock-moment-info';
 
 describe('StockMomentInfo', () => {
   it('should create an instance', () => {
-    expect(new StockMomentInfo()).toBeTruthy();
+    expect(new StockMomentInfo(new Date(Date.now()), '1', '1', '1', '1')).toBeTruthy();
   });
 });
