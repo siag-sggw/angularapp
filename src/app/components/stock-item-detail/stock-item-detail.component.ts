f619ba45af4bd3673ce243cb582001b8d3ad52c5
WiktorElAttar <wiktor.elattar@gmail.com>
2019-05-10 14:01:10 +0200

    Added FusionCharts

--- src/app/components/stock-item-detail/stock-item-detail.component.ts
+++ src/app/components/stock-item-detail/stock-item-detail.component.ts
@@ -7,11 +7,66 @@ import { StockItem } from 'src/app/models/stock-item.model';
   styleUrls: ['./stock-item-detail.component.scss']
 })
 export class StockItemDetailComponent implements OnInit {
-  @Input() stockItem: StockItem;
+  private _stockItem: StockItem;
+  @Input() set stockItem(value: StockItem) {
+    this._stockItem = value;
+    this.setChartData();
+  }
 
-  constructor() { }
+  get stockItem(): StockItem {
+    return this._stockItem;
+  }
+
+  dataSource: Object;
+  chartConfig: Object;
+  stockData;
+
+  constructor() {
+    this.stockData = [];
+   }
 
   ngOnInit() {
+    this.chartConfig = {
+      width: '100%',
+      height: '400',
+      type: 'candlestick',
+      dataFormat: 'json'
+    };
+
+    this.setChartData();
+  }
+
+  private setChartData() {
+    let x = 1;
+    this.stockData = [];
+    this.stockItem.data.forEach( el => {
+      let temp = {
+        "open": el.open.toString(),
+        "high": el.high.toString(),
+        "low": el.low.toString(),
+        "close": el.close .toString(),
+        "x": x++
+      };
+      this.stockData.push(temp);
+    });
+
+    let symbol = this.stockItem.symbol;
+
+    this.dataSource = {
+      "chart": {
+        "caption": symbol.toUpperCase(),
+        "subcaption": "subcaption",
+        "numberprefix": "$",
+        "pyaxisname": "Price (USD)",
+        "theme": "fusion",
+        "showvolumechart": "0",
+      },
+      "dataset": [
+        {
+          "data": this.stockData
+        }
+      ]
+    }
   }
 
 }
